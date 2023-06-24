f6e8a9e1366d58d609588eac0bcc23a30aead113
kacou48 <63119982+kacou48@users.noreply.github.com>
2023-06-24 08:49:52 +0100

    Update keys.js
    Update keys

--- static/js/keys.js
+++ static/js/keys.js
@@ -1,4 +1,4 @@
-const api_key='902bf69dc9d8a758204c2e820f7e09f22c6d214abe644dee137b3a176034aea1';
-const x_api_key = "aa6f8c9a-2453-4b5d-b13a-80edc4018ea8";
-const api_keyFiat = 'pk_prod_8Ulu_P5yaGjVk7waOoyzPz9YgSVXoksBS6IF5RHfBW40';
+const api_key='api_key';
+const x_api_key = "x_api_key";
+const api_keyFiat = 'api_kayFiat';
 
